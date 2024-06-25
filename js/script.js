398cd952d506c2fe48a75301e781c4878ab960bf
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-06-25 17:18:36 -0600

    Added basic math functions for calculator

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Math functions
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function sqrRoot(a) {
+  return Math.sqrt(a);
+}
+
+function percentToDecimal(a) {
+  return a / 100;
+}
